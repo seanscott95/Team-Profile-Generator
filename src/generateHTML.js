f07fc3a2b359622048788916e8859ff4a573fb46
Sean <seanms418@gmail.com>
2022-02-04 18:49:46 +1100

    completed the generate intern and engineer functions in generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -15,3 +15,38 @@ const generateManager = (manager) => {
                     </div>
                 </article>`;
 }
+
+const generateEngineer = (engineer) => {
+    return `<article class="col-4 mt-3">
+                <div class="card">
+                    <section class="card-header bg-primary">
+                        <h2 class="text-white">${engineer.name}</h2>
+                        <h3 class="text-white"><i class="bi bi-laptop"></i> Engineer</h3>
+                    </section>
+                    <section class="card-body bg-light">
+                        <p class="p-2 m-1 bg-white border">ID: ${engineer.id}</p>
+                        <p class="p-2 m-1 bg-white border">Email: <a
+                                href="mailto:${engineer.email}">${engineer.email}</a></p>
+                        <p class="p-2 m-1 bg-white border">GitHub: ${engineer.github}</p>
+                    </section>
+                </div>
+            </article>`;
+}
+
+const generateIntern = (intern) => {
+    return `<article class="col-4 mt-3">
+    <div class="card">
+        <section class="card-header bg-primary">
+            <h2 class="text-white">${intern.name}</h2>
+            <h3 class="text-white"><i class="bi bi-book"></i> Intern</h3>
+        </section>
+
+        <section class="card-body bg-light">
+            <p class="p-2 m-1 bg-white border">ID: ${intern.id}</p>
+            <p class="p-2 m-1 bg-white border">Email: <a
+                    href="mailto:${intern.email}">${intern.email}</a></p>
+            <p class="p-2 m-1 bg-white border">School: ${intern.school}</p>
+        </section>
+    </div>
+</article>`;
+}
