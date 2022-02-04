a0c81e22aa49985d68cc9c07aaeacbe56c3f7f9f
Sean <seanms418@gmail.com>
2022-02-04 19:35:35 +1100

    added links at top of page

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTMLPage = require("./src/generateHTML");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const cardArray = [];
+
+
