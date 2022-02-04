d8b1de2a0965dfafe0c32872f3c15267456b9f81
Sean <seanms418@gmail.com>
2022-02-04 18:55:55 +1100

    finished the generateHTMLPage function

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -50,3 +50,37 @@ const generateIntern = (intern) => {
     </div>
 </article>`;
 }
+
+const generateHTMLPage = (cards) => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+        <title>My team</title>
+    </head>
+    
+    <body>
+        <header class="text-center text-white bg-primary p-3 mb-5 display-3">My Team</header>
+    
+        <main>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <!--CARDS ADDED HERE-->
+                    ${cards}
+                </div>
+            </div>
+        </main>
+    </body>
+    
+    </html>`;
+}
+
+const generateTeamPage = () => {
+    
+}
