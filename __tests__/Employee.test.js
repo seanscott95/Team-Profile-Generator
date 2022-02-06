0770aadc59206646c865bc562ef74b71c8851b93
Sean <seanms418@gmail.com>
2022-02-06 12:52:09 +1100

    modified jest into devDependencies, created first failing test for employee

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,15 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    describe("constructor function with name, id, email as properties, getName, getID, getEmail, getRole as methods", () => {
+        it("properties for name, id and email", () => {
+            const newEmployee = new Employee("james", 1, "fake@fakemail.com")
+            expect(newEmployee.name).toEqual("james");
+            expect(newEmployee.id).toEqual("1")
+            expect(newEmployee.email).toEqual("fake@fakemail.com")
+        });
+        // it("should have getName, getId, getEmail, getRole as methods", () => {
+
+        // })
+    });
+})
