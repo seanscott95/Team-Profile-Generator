a02b366c2f9ad9bb38139a9906801ee7ab3d8163
Sean <seanms418@gmail.com>
2022-02-06 18:58:07 +1100

    added comments

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+// Node modules
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+// Generates HTML
 const generateHTMLPage = require("./src/generateHTML");
+
+// Team profiles
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+// Creates a new cardArray to be pushed to
 const cardArray = [];
 
+// Function that has uses the fs writeFile function
 const writeToFile = (data) => {
     fs.writeFile("./dist/index.html", data, (err) => {
         err ? console.log(err) : console.log("Success! You have created a team profile named index.html")
     });
 }
 
+// Questions for the engineer
 const engineerQuestions = () => {
     return inquirer
         .prompt([
@@ -34,6 +42,7 @@ const engineerQuestions = () => {
                 name: "id",
                 message: "What is the engineers employee ID?",
                 validate: idInput => {
+                    // Checks to se if the input is a number or not
                     if (isNaN(idInput)) {
                         console.log('Please enter a number.')
                         return false;
@@ -46,6 +55,7 @@ const engineerQuestions = () => {
                 type: "input",
                 name: "email",
                 message: "What is the engineers email?",
+                // Checks to see if the input is a valid email
                 validate: email => {
 
                     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
@@ -73,13 +83,18 @@ const engineerQuestions = () => {
             }
         ])
         .then((engineerData) => {
+            // Creates new constants with values from the data
             const { name, id, email, github } = engineerData;
+            // Uses those values to and inputs them into a new Engineer class
             const engineer = new Engineer(name, id, email, github);
+            // Adds the new Engineer to the cardArray
             return cardArray.push(engineer);
         })
+        // Goes back to init acting like a menu for questions
         .then(init)
 }
 
+// Questions for the intern
 const internQuestions = () => {
     return inquirer
         .prompt([
@@ -101,6 +116,7 @@ const internQuestions = () => {
                 name: "id",
                 message: "What is the interns employee ID",
                 validate: idInput => {
+                    // Checks to see if the input is a number or not
                     if (isNaN(idInput)) {
                         console.log('Please enter a number.')
                         return false;
@@ -113,6 +129,7 @@ const internQuestions = () => {
                 type: "input",
                 name: "email",
                 message: "What is the interns email?",
+                // Checks to see if the input is a valid email
                 validate: email => {
                     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
                     if (valid) {
@@ -138,10 +155,14 @@ const internQuestions = () => {
             }
         ])
         .then((internData) => {
+            // Creates new constants with values from the data
             const { name, id, email, school } = internData;
+            // Uses those values to and inputs them into a new Engineer class
             const intern = new Intern(name, id, email, school);
+            // Adds the new Engineer to the cardArray
             return cardArray.push(intern);
         })
+        // Goes back to init acting like a menu for questions
         .then(init)
 }
 
@@ -166,6 +187,7 @@ const managerQuestions = () => {
                 name: "id",
                 message: "What is the managers employee ID?",
                 validate: idInput => {
+                    // Checks to see if the input is a number or not
                     if (isNaN(idInput)) {
                         console.log('Please enter a number.')
                         return false;
@@ -178,6 +200,7 @@ const managerQuestions = () => {
                 type: "input",
                 name: "email",
                 message: "What is the managers email?",
+                // Checks to see if the input is a valid email
                 validate: email => {
                     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
                     if (valid) {
@@ -193,6 +216,7 @@ const managerQuestions = () => {
                 name: "office",
                 message: "What is the managers office number?",
                 validate: officeNumberInput => {
+                    // Checks to see if the input is a valid number
                     if (isNaN(officeNumberInput)) {
                         console.log('Please enter a number.')
                         return false;
@@ -203,12 +227,16 @@ const managerQuestions = () => {
             }
         ])
         .then((managerData) => {
+            // Creates new constants with values from the data
             const { name, id, email, office } = managerData;
+            // Uses those values to and inputs them into a new Engineer class
             const manager = new Manager(name, id, email, office);
+            // Adds the new Engineer to the cardArray
             return cardArray.push(manager);
         })
 }
 
+// Questions for the menu
 const init = () => {
     return inquirer
         .prompt([
@@ -222,17 +250,23 @@ const init = () => {
         .then((data) => {
             const role = data.role;
             if (role === "Engineer") {
+                // If engineer is inputted then it goes to the engineer questions
                 return engineerQuestions();
+                // If engineer is inputted then it goes to the engineer questions
             } else if (role === "Intern") {
                 return internQuestions();
+                // If finish is chosen than it returns the cardArray ending the application
             } else if (role === "Finish") {
                 return cardArray;
             }
         });
 }
 
+// Starts the application with the managerQuestions then onto the init (menu)
 managerQuestions()
     .then(init)
+    // Adds the cards to generate the HTML page text
     .then(cardArray => generateHTMLPage(cardArray))
+    // Uses the generated HTML page text to generate an index.html file
     .then(HTMLPage => writeToFile(HTMLPage))
     .catch(err => console.log(err));
