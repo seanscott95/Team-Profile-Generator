ce1951e525e13bd231de6d469196aa3ab28be36e
Sean <seanms418@gmail.com>
2022-02-06 13:07:31 +1100

    second test created failed and then passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,13 +3,24 @@ const Employee = require("../lib/Employee");
 describe("Employee", () => {
     describe("constructor function with name, id, email as properties, getName, getID, getEmail, getRole as methods", () => {
         it("properties for name, id and email", () => {
-            const newEmployee = new Employee("james", 1, "fake@fakemail.com")
+            let newEmployee = new Employee("james", 1, "fake@fakemail.com");
             expect(newEmployee.name).toEqual("james");
             expect(newEmployee.id).toEqual(1);
             expect(newEmployee.email).toEqual("fake@fakemail.com");
         });
-        // it("should have getName, getId, getEmail, getRole as methods", () => {
-
-        // })
+        it("should return name in getName()", () => {
+            let newEmployee = new Employee("james", 1, "fake@fakemail.com");
+            let returnName = newEmployee.getName();
+            expect(returnName).toEqual("james");
+        });
+        // it("should return the id in getId()", () => {
+            
+        // });
+        // it("should return the email in getEmail()", () => {
+            
+        // });
+        // it("should return the role in getRole()", () => {
+            
+        // });
     });
 })
