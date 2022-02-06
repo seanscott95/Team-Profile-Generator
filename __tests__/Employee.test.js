6d1ae45125ce52c0bedcb82b5bd6cd8930cc7010
Sean <seanms418@gmail.com>
2022-02-06 12:57:54 +1100

    test had quotations around a number so it failed, Employee.js wasnt being exported, both working and test passes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,8 +5,8 @@ describe("Employee", () => {
         it("properties for name, id and email", () => {
             const newEmployee = new Employee("james", 1, "fake@fakemail.com")
             expect(newEmployee.name).toEqual("james");
-            expect(newEmployee.id).toEqual("1")
-            expect(newEmployee.email).toEqual("fake@fakemail.com")
+            expect(newEmployee.id).toEqual(1);
+            expect(newEmployee.email).toEqual("fake@fakemail.com");
         });
         // it("should have getName, getId, getEmail, getRole as methods", () => {
 
