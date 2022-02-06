12af7282e1d0ab4c8ab7f5140c30e9e6168662b7
Sean <seanms418@gmail.com>
2022-02-06 14:22:35 +1100

    added the test for getRole in manager, added code, test passes, manager finished

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -6,4 +6,9 @@ describe(("Manager"), () => {
         let officeNumber = newManager.officeNumber;
         expect(officeNumber).toBe(10);
     });
+    it(("should return role in getRole()"), () => {
+        let newManager = new Manager("james", 1, "fake@fakemail.com", 10);
+        let role = newManager.getRole();
+        expect(role).toBe("Manager");
+    });
 })
