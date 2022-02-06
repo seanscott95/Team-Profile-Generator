70bfe440acfb01f8730f6c1c01cc3a1c9ea25d0f
Sean <seanms418@gmail.com>
2022-02-06 15:06:45 +1100

    intern tests for getSchool and getRole created, code added, tested, passed, intern finished

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -5,5 +5,15 @@ describe(("Intern"), () => {
         let newIntern = new Intern("james", 1, "fake@fakemail.com", "Fakeschool High");
         let internSchool = newIntern.school;
         expect(internSchool).toEqual("Fakeschool High");
-    })
+    });
+    it(("Should return school from getSchool()"), () => {
+        let newIntern = new Intern("james", 1, "fake@fakemail.com", "Fakeschool High");
+        let getSchool = newIntern.getSchool();
+        expect(getSchool).toEqual("Fakeschool High");
+    });
+    it(("Should return role from getRole()"), () => {
+        let newIntern = new Intern("james", 1, "fake@fakemail.com", "Fakeschool High");
+        let role = newIntern.getRole();
+        expect(role).toEqual("Intern");
+    });
 })
